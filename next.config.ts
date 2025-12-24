2cb79dcd076567a3cc4fc4c1a8a9d6397d71ef25
Narasena <cursedsena@gmail.com>
2025-12-24 16:34:40 +0700

    feat(menu): create menu cards

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mykoreankitchen.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.cucinabyelena.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.marionskitchen.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { IconSearch } from "@tabler/icons-react"
+import Image from "next/image"
 import { useState } from "react"
 
 export default function MenuPage() {
@@ -8,10 +9,59 @@ export default function MenuPage() {
 
     const tabs = ["Hot Dishes", "Cold Dishes", "Soup", "Grill", "Appetizer", "Desert"]
     const [activeTab, setActiveTab] = useState(tabs[0])
-    const handleChangeTab = (tab:string) => {
+    const handleChangeTab = (tab: string) => {
         console.log(tab)
         setActiveTab(tab)
     }
+
+    const menus = [
+        {
+            type: "Hot Dishes",
+            items: [
+                {
+                    imgUrl: "https://mykoreankitchen.com/wp-content/uploads/2016/04/2.-Jjampong-500x375.jpg",
+                    dishName: "Spicy seasoned seafood noodles",
+                    price: 2.29,
+                    stock: 20
+                },
+                {
+                    imgUrl: "https://www.cucinabyelena.com/wp-content/uploads/2025/01/Creamy-Mushroom-Pasta-Recipe-21.jpg",
+                    dishName: "Salted pasta with mushroom sauce",
+                    price: 2.69,
+                    stock: 11
+                },
+                {
+                    imgUrl: "https://www.marionskitchen.com/wp-content/uploads/2019/12/Mushroom-Dumpling-Hot-Sour-Soup2.jpg",
+                    dishName: "Beef dumpling in hot and sour sauce",
+                    price: 2.99,
+                    stock: 16
+                },
+            ]
+        },
+        {
+            type: "Cold Dishes",
+            items: [
+                {
+                    imgUrl: "",
+                    dishName: "Cold spicy seasoned seafood noodles",
+                    price: 2.29,
+                    stock: 20
+                },
+                {
+                    imgUrl: "",
+                    dishName: "Cold salted pasta with mushroom sauce",
+                    price: 2.69,
+                    stock: 11
+                },
+                {
+                    imgUrl: "",
+                    dishName: "Cold beef dumpling in hot and sour sauce",
+                    price: 2.99,
+                    stock: 16
+                },
+            ]
+        },
+    ]
 
     return (
         <div className="bg-darkbg1 rounded-2xl p-4 min-w-full min-h-full">
@@ -43,8 +93,33 @@ export default function MenuPage() {
             {/* tabs  */}
             <div className="flex mt-4 text-white border-b border-white/50">
                 {tabs.map((tab, index) => (
-                    <div key={index} onClick={()=>handleChangeTab(tab)} className={`px-3 pb-1 hover:bg-primary ${activeTab===tab ? "text-primary border-b-2 border-primary font-semibold" : ""}`}>{tab}</div>
+                    <div key={index} onClick={() => handleChangeTab(tab)} className={`px-3 pb-1 hover:bg-primary ${activeTab === tab ? "text-primary border-b-2 border-primary font-semibold" : ""}`}>{tab}</div>
                 ))}
+            </div>
+
+            {/* dishes cards  */}
+            <div className="px-4 mt-4">
+                <div className="flex items-center justify-between">
+                    <h1>Choose Dishes</h1>
+                    <select name="" id="">
+                        <option value="">Dine In</option>
+                        <option value="">Take Away</option>
+                    </select>
+                </div>
+                <div className="grid grid-cols-5 gap-5">
+                    {menus.find(menu => menu.type === activeTab)?.items.map((item, index) => (
+                        <div key={index} className="bg-formbg flex flex-col gap-1 items-center text-white">
+                            {item.imgUrl !== "" ? (
+                                <Image src={item.imgUrl} alt={item.dishName} width={132} height={132} />
+                            ) : (
+                                <div className="size-33 rounded-full bg-primary"></div>
+                            )}
+                            <h3 className="centered">{item.dishName}</h3>
+                            <p>{item.price}</p>
+                            <p>{item.stock} Bowls available</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
