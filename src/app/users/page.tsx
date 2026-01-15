ec14669923d3c1e10532f298f27e71faef98c71f
Narasena <cursedsena@gmail.com>
2026-01-15 10:16:47 +0700

    feat(users): next ssr

---
+++ src/app/users/page.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { PlusIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemMedia,
+  ItemSeparator,
+  ItemTitle,
+} from "@/components/ui/item";
+
+export default async function UsersPage() {
+  const data = await fetch("https://dummyjson.com/users", {
+    cache: "no-store",
+  }).then((res) => res.json());
+  const people = await data.users;
+
+  return (
+    <div className="flex w-full max-w-md flex-col gap-6">
+      <ItemGroup>
+        {people.map((person:any, index:number) => (
+          <React.Fragment key={person.username}>
+            <Item>
+              <ItemMedia>
+                <Avatar>
+                  <AvatarImage src={person.image} className="grayscale" />
+                  <AvatarFallback>{person.username.charAt(0)}</AvatarFallback>
+                </Avatar>
+              </ItemMedia>
+              <ItemContent className="gap-1">
+                <ItemTitle>{person.username}</ItemTitle>
+                <ItemDescription>{person.email}</ItemDescription>
+              </ItemContent>
+              <ItemActions>
+                <Button variant="ghost" size="icon" className="rounded-full">
+                  <PlusIcon />
+                </Button>
+              </ItemActions>
+            </Item>
+            {index !== people.length - 1 && <ItemSeparator />}
+          </React.Fragment>
+        ))}
+      </ItemGroup>
+    </div>
+  );
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,9 +2,17 @@
 
 import { IconBuildingStore, IconBurger, IconDashboard, IconLogout, IconSettingsFilled } from "@tabler/icons-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function SideBar() {
+  const [user, setUser] = useState<any>(null)
+  const handleFetchUserCredential = async () => {
+    fetch(`https://dummyjson.com/users/16`).then(res => res.json()).then(setUser)
+  }
+  useEffect(()=>{
+    handleFetchUserCredential()
+  },[])
   const sideBarNavs = {
     header: {
       icon: <IconBuildingStore />,
@@ -36,24 +44,30 @@ export default function SideBar() {
     setSidebarOpen(!sidebarOpen)
   }
   return (
-    <div className={`sm:w-[${sidebarOpen ? "300" : "60"}px] h-screen bg-darkbg2 p-4 flex flex-col justify-between`}>
+    <div
+      className={`sm:w-[${
+        sidebarOpen ? "300" : "60"
+      }px] h-screen bg-darkbg2 p-4 flex flex-col justify-between`}
+    >
       <div>
-
         {/* header  */}
-        <button onClick={handleSidebarOpen} className="centered gap-3 w-full font-semibold text-2xl bg-accent1 hover:bg-accent1/70 py-2 rounded-md">
+        <button
+          onClick={handleSidebarOpen}
+          className="centered gap-3 w-full font-semibold text-2xl bg-accent1 hover:bg-accent1/70 py-2 rounded-md"
+        >
           <div className="*:size-10">{sideBarNavs.header.icon}</div>
-          {sidebarOpen && (
-            <div>{sideBarNavs.header.title}</div>
-          )}
+          {sidebarOpen && <div>{sideBarNavs.header.title}</div>}
         </button>
         {/* nav  */}
         <div className="my-3 py-4 flex flex-col gap-y-5">
           {sideBarNavs.navigations.map((nav, index) => (
-            <Link key={index} href={nav.url} className="flex gap-x-2 py-1.5 hover:bg-primary rounded-md px-3 hover:font-bold hover:text-white">
+            <Link
+              key={index}
+              href={nav.url}
+              className="flex gap-x-2 py-1.5 hover:bg-primary rounded-md px-3 hover:font-bold hover:text-white"
+            >
               {nav.icon}
-              {sidebarOpen && (
-                <span>{nav.title}</span>
-              )}
+              {sidebarOpen && <span>{nav.title}</span>}
             </Link>
           ))}
         </div>
@@ -62,15 +76,22 @@ export default function SideBar() {
       <div className="flex justify-between text-white bg-darkbg1 border-darkline rounded-lg border p-1.5">
         {sidebarOpen && (
           <div className="flex text-sm font-medium gap-2">
-            <div className="size-10 bg-primary rounded-full centered">AV</div>
+            <Avatar>
+              <AvatarImage src={user && user.image} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
             <div>
-              <p>Agus Suratno</p>
-              <span className="font-light text-xs text-grayfont">admin01@mail.com</span>
+              <p>{user && user.firstName+" "+user.lastName}</p>
+              <span className="font-light text-xs text-grayfont">
+                {user && user.email}
+              </span>
             </div>
           </div>
         )}
-        <button><IconLogout /></button>
+        <button>
+          <IconLogout />
+        </button>
       </div>
     </div>
-  )
+  );
 }
